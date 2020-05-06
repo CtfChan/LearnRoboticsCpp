531624c475e8db73c327566b778ae7034a45bf61
CtfChan <ctf.chan@mail.utoronto.ca>
2020-05-07 03:59:58 +0800

    finishing state lattice

--- include/state_lattice.hpp
+++ include/state_lattice.hpp
@@ -4,12 +4,56 @@
 
 #include "trajectory_optimizer.hpp"
 
+/**
+ * @brief 
+ * 
+ * @param angle_samples this parameter influences density of samples at each angle
+ * @param a_min 
+ * @param a_max 
+ * @param d 
+ * @param p_max 
+ * @param p_min 
+ * @param nh 
+ * @return std::vector<Pose2D> 
+ */
+std::vector<Pose2D> sampleStates(std::vector<float>& angle_samples, float a_min,
+                 float a_max, float d, float p_max, float p_min, size_t nh);
 
-std::vector<Pose2D> sampleStates();
+/**
+ * @brief Calculate uniform states.
+ * 
+ * @param nxy number of positions to sample (np in paper)
+ * @param nh number of headings to sample
+ * @param d distance of terminal states
+ * @param a_min position sampling min angle
+ * @param a_max position sampling max angle
+ * @param p_min heading sampling min angle
+ * @param p_max heading sampling max angle
+ * @return std::vector<Pose2D> 
+ */
+std::vector<Pose2D> calculateUniformPolarStates(size_t nxy, size_t nh, float d,
+                        float a_min, float a_max, float p_min, float p_max);
 
-std::vector<Pose2D> calculateUniformPolarStates();
 
-std::vector<Pose2D> calculateBiasedPolarStates();
+
+/**
+ * @brief 
+ * 
+ * @param goal_angle 
+ * @param ns 
+ * @param nxy 
+ * @param nh 
+ * @param d 
+ * @param a_min 
+ * @param a_max 
+ * @param p_min 
+ * @param p_max 
+ * @return std::vector<Pose2D> 
+ */
+std::vector<Pose2D> calculateBiasedPolarStates(float goal_angle, int ns, int nxy,
+                                   int nh, int d,
+                                   float a_min, float a_max,
+                                   float p_min, float p_max);
 
 
 /**
@@ -25,6 +69,7 @@ std::vector<Pose2D> calculateBiasedPolarStates();
  */
 std::vector<Pose2D> calculateLaneStates(float l_center, float l_heading, 
                             float l_width, float v_width, float d, size_t nxy);
+
 
 
 
--- src/state_lattice.cpp
+++ src/state_lattice.cpp
@@ -2,6 +2,87 @@
 
 #include "state_lattice.hpp"
 
+#include <numeric>
+
+
+std::vector<Pose2D> sampleStates(std::vector<float>& angle_samples, float a_min,
+                 float a_max, float d, float p_max, float p_min, size_t nh) {
+    std::vector<Pose2D> states; 
+    
+    float xf, yf, yawf;
+    for (float i : angle_samples) {
+        float a = a_min + (a_max - a_min) * i;
+        for (int j = 0; j < nh; ++j) {
+            xf = d * std::cos(a);
+            yf = d * std::sin(a);
+            if (nh == 1)
+                yawf = (p_max - p_min) / 2.f + a;
+            else 
+                yawf = p_min + (p_max - p_min) * j / (nh - 1.f) + a;
+            Pose2D final_state {xf, yf, yawf};
+            states.push_back(final_state);
+        }
+    }
+
+    return states;
+}
+
+
+std::vector<Pose2D> calculateUniformPolarStates(size_t nxy, size_t nh, float d,
+                        float a_min, float a_max, float p_min, float p_max) {
+
+    std::vector<float> angle_samples(nxy);
+    for (int i = 0; i < nxy; ++i)
+        angle_samples[i] = i / (nxy - 1.f);
+
+    std::vector<Pose2D> states = sampleStates(angle_samples, a_min, a_max, d, p_max, p_min, nh);
+    
+    return states;
+}
+
+
+std::vector<Pose2D> calculateBiasedPolarStates(float goal_angle, int ns, int nxy,
+                                   int nh, int d,
+                                   float a_min, float a_max,
+                                   float p_min, float p_max){
+    std::vector<float> as(ns-1);
+    std::vector<float> cnav(ns-1);
+    for (size_t i = 0; i < ns - 1; ++i) {
+        as[i] = a_min + (a_max - a_min) * i / (ns - 1.f);
+        cnav[i] = M_PI - std::abs(as[i] - goal_angle);
+    }
+
+    float cnav_sum = std::accumulate(cnav.begin(), cnav.end(), 0.f);
+    float cnav_max = *std::max_element(cnav.begin(), cnav.end());
+
+    // normalise
+    for (float& cnavi : cnav) {
+        cnavi = (cnav_max - cnavi) / (cnav_max * ns - cnav_sum);
+    }
+
+    // cumalative sum
+    std::vector<float> cumsum_cnav(cnav.size());
+    std::partial_sum(cnav.begin(), cnav.end(), cumsum_cnav.begin());
+
+    // output angles 
+    std::vector<float> di;
+    int li = 0;
+    for (int i = 0; i < nxy; ++i ) {
+        for (int ii = li; ii < ns-1; ++ii) {
+            if ( ii*(1.f/ ns) >= i / (nxy - 1.f) ) {
+                di.push_back(cumsum_cnav[ii]);
+                li = ii - 1;
+                break;
+            }
+        }
+    }
+    
+    auto states = sampleStates(di, a_min, a_max, d, p_max, p_min, nh);
+    return states;  
+}
+
+
+
 std::vector<Pose2D> calculateLaneStates(
     float l_center, float l_heading, float l_width, 
     float v_width, float d, size_t nxy) {
@@ -50,19 +131,6 @@ std::pair<std::vector<TableEntry>, Arrow> generatePaths(TrajectoryOptimizer& opt
         }
     }
 
-
     return {table, all_arr};
 }
-
-// // trajectory is robot centric, so start of optimization always at (0, 0, 0)
-//     // (x, y, yaw, v, wheel_base, max_steer)
-//     BicycleModelRobot state{0.f, 0.f, 0.f, 3.0f, 1.0f, deg2rad(90.0f)};
-//     float k0 = 0.0f; // initial steer
-
-//     // initialize optimizer
-//     size_t max_iter = 100;
-//     float cost_th = 0.1f;
-//     TrajectoryParam delta_param{0.5f, 0.02f, 0.02f};
-//     float ds = 0.1f; // discretization of path length for trajectory generation
-//     TrajectoryOptimizer optim(state, max_iter, cost_th, delta_param, ds);
 
