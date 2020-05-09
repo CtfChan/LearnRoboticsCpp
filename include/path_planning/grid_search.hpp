9ac69675b8a2ff0d29726eb8430e3b06dae8ab79
CtfChan <ctf.chan@mail.utoronto.ca>
2020-05-09 22:47:04 +0800

    moving definitions to grid_search.cpp

--- include/path_planning/grid_search.hpp
+++ include/path_planning/grid_search.hpp
@@ -23,109 +23,29 @@ struct Node {
 class GridSearchBase {
 public:
     GridSearchBase (std::vector<int>& ox,  std::vector<int>& oy, 
-             float grid_res,  float robot_radius) : 
-            grid_res_(grid_res), robot_radius_(robot_radius),
-            ox_(ox), oy_(oy) {
-        calculateObstacleMap(ox, oy);
-    }
+             float grid_res,  float robot_radius);
 
     virtual std::pair<std::vector<int>, std::vector<int>> 
         plan(float sx, float sy, float gx, float gy) = 0;
 
-    std::pair<std::vector<int>, std::vector<int>> getExpandedNodes() {
-        return {expanded_x_, expanded_y_};
-    }
+    std::pair<std::vector<int>, std::vector<int>> getExpandedNodes();
 
 protected:
-    void calculateObstacleMap( std::vector<int>& ox,  std::vector<int>& oy) {
-        auto [min_x, max_x] = std::minmax_element(ox.begin(), ox.end());
-        min_x_ = std::round(*min_x);
-        max_x_ = std::round(*max_x);
+    void calculateObstacleMap( std::vector<int>& ox,  std::vector<int>& oy);
 
-        auto [min_y, max_y] = std::minmax_element(oy.begin(), oy.end());
-        min_y_ = std::round(*min_y);
-        max_y_ = std::round(*max_y);
+    std::pair<std::vector<int>, std::vector<int>> 
+        calculateFinalPath(Node goal_node, std::unordered_map<int, Node>& closed_set) ;
 
-        x_width_ = std::round( (max_x_ - min_x_ ) /   grid_res_     );
-        y_width_ = std::round( (max_y_ - min_y_ ) /   grid_res_     );
+    // converts position along X/Y axis into index
+    int calculateXYIndex(int pos, int min_pos) ;
 
-        obstacle_map_ = std::vector<std::vector<bool>>(x_width_, std::vector<bool>(y_width_, false));
-        // apply inflation
-        // at each grid cell iterate through all obstacles and check if within radius
-        int iox, ioy; // obs grid cell
-        int x, y; // grid cell
-        float d;
-        for (int ix = 0; ix < x_width_; ++ix) {
-            x = calculatePosition(ix, min_x_);
-            for (int iy = 0; iy < y_width_; ++iy) {
-                y = calculatePosition(iy, min_y_);
+    // converts (x,y) into 1D index
+    int calculateIndex(Node& node);
 
-                for (size_t obs_num = 0; obs_num < ox.size(); ++obs_num) {
-                    iox = ox[obs_num], ioy = oy[obs_num];
-                    d = std::hypot(iox - x, ioy - y);
-                    if (d <= robot_radius_) {
-                        obstacle_map_[ix][iy] = true;
-                        break;
-                    }
-                }
-            }
-        }
-    }
+    // convert index position along X/Y axis into actual position
+    float calculatePosition(int index, float minp);
 
-   std::pair<std::vector<int>, std::vector<int>> calculateFinalPath(Node goal_node, 
-                                            std::unordered_map<int, Node>& closed_set) {
-         // for plotting   
-        std::vector<int> rx;
-        std::vector<int> ry;  
-
-        rx.push_back(calculatePosition(goal_node.x, min_x_)); 
-        ry.push_back(calculatePosition(goal_node.y, min_y_));
-        int parent = goal_node.parent;
-
-        while (parent != -1) {
-            Node n = closed_set[parent];
-            rx.push_back(calculatePosition(n.x, min_x_));
-            ry.push_back(calculatePosition(n.y, min_y_));
-            parent = n.parent;
-        }
-
-        return std::make_pair(rx, ry);                                        
-    }
-
-
-    int calculateXYIndex(int pos, int min_pos) {
-        return std::round((pos - min_pos) / grid_res_);
-    }
-
-
-    int calculateIndex(Node& node) {
-        return (node.y - min_y_) * x_width_ + (node.x - min_x_);
-    }
-
-    float calculatePosition(int index, float minp) {
-        float pos = index * grid_res_ + minp;
-        return pos;
-    }
-
-    bool verifyNode(Node& node) {
-        float px = calculatePosition(node.x, min_x_);
-        float py = calculatePosition(node.y, min_y_);
-
-        if (px < min_x_)
-            return false;
-        if (py < min_y_)
-            return false;
-        if (px >= max_x_)
-            return false;
-        if (py >= max_y_)
-            return false;
-
-        if (obstacle_map_[node.x][node.y])
-            return false;
-
-        return true;
-    }
-
+    bool verifyNode(Node& node);
 
     std::vector<int> expanded_x_;
     std::vector<int> expanded_y_;
@@ -159,83 +79,31 @@ protected:
 };
 
 
+
+
 class Dijkstra : public GridSearchBase {
 public:
     Dijkstra(std::vector<int>& ox,  std::vector<int>& oy, 
-             float grid_res,  float robot_radius) : 
-                GridSearchBase(ox, oy, grid_res, robot_radius) {}
+             float grid_res,  float robot_radius);
 
     virtual std::pair<std::vector<int>, std::vector<int>> 
-        plan(float sx, float sy, float gx, float gy) override {
+        plan(float sx, float sy, float gx, float gy) override;
 
-    Node start_node(calculateXYIndex(sx, min_x_), 
-                    calculateXYIndex(sy, min_y_), 0.0f, -1);
-    Node goal_node(calculateXYIndex(gx, min_x_), 
-                    calculateXYIndex(gy, min_y_), 0.0f, -1);
-    
-    std::unordered_map<int, Node> open_set;
-    std::unordered_map<int, Node> closed_set;
-
-    open_set[calculateIndex(start_node)] = start_node;
+};
 
 
-    while (true) {
-        auto next_node = std::min_element(open_set.begin(), open_set.end(), 
-        [&](const auto& p1, const auto& p2){
-            return p1.second.cost < p2.second.cost;
-        });
-
-        int c_id = (*next_node).first;
-        Node current = (*next_node).second;
-
-        
-        float wx = calculatePosition(current.x, min_x_);
-        float wy = calculatePosition(current.y, min_y_);
-        expanded_x_.push_back(wx);
-        expanded_y_.push_back(wy);
-        // gp << "plot '-' title 'obs', '-' title 'expanded'\n";
-        // gp.send1d(boost::make_tuple(ox_, oy_));
-        // gp.send1d(boost::make_tuple(expanded_x, expanded_y));
 
 
-        // check goal
-        if (current.x == goal_node.x && current.y == goal_node.y) {
-            std::cout << "Goal found!" << std::endl;
-            goal_node.parent = current.parent;
-            goal_node.cost = current.cost;
-            break;
-        }
+class Astar : public GridSearchBase {
+public:
+    Astar(std::vector<int>& ox,  std::vector<int>& oy, 
+             float grid_res,  float robot_radius);
 
-        // remove from open set and add to closed set
-        open_set.erase(c_id);
-        closed_set[c_id] = current;
+    virtual std::pair<std::vector<int>, std::vector<int>> 
+        plan(float sx, float sy, float gx, float gy) override;
 
-        // visit neighbours
-        for (auto [dx, dy, cost] : motion_model_) {
-            Node node(current.x + dx, current.y + dy, current.cost + cost, c_id);
-            int n_id = calculateIndex(node);
-
-            if (closed_set.find(n_id) != closed_set.end()) 
-                continue;
-
-            if (verifyNode(node) == false)
-                continue;
-
-            // add to open set if not there, update open set if better cost found
-            if (open_set.find(n_id) == open_set.end() || open_set[n_id].cost >= node.cost) {
-                open_set[n_id] = node;
-            } 
-        }
-    }
- 
-    auto [rx, ry] = calculateFinalPath(goal_node, closed_set);
-    // gp << "plot '-' title 'obs', '-' title 'path'\n";
-    // gp.send1d(boost::make_tuple(ox_, oy_));
-    // gp.send1d(boost::make_tuple(rx, ry));
-
-    return {rx, ry};
-
-        // return {{}, {}};
-    }
+private:
+    // euclidean distance as heuristic
+    float calculateHeuristic(const Node& n1, const Node& n2);
 
 };
